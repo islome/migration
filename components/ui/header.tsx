3247f6de763be8ac8e490bcfebcbe574e4f72e93
islome <islombekkamoliddinoff05@gmail.com>
2026-02-04 19:59:05 +0500

    header component improved for phone

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -9,8 +9,8 @@ export default function Header() {
       <header className="bg-white/80 backdrop-blur-md shadow-sm fixed left-0 w-full top-0 z-50">
         <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
           <div className="flex items-center space-x-2">
-            <Factory className="w-8 h-8 text-black" />
-            <span className="text-2xl font-bold bg-black bg-clip-text text-transparent">
+            <Factory className="w-8 h-8 text-black hidden md:block" />
+            <span className="text-xl md:text-2xl font-bold bg-black bg-clip-text text-transparent">
               Best Globalize
             </span>
           </div>
@@ -35,7 +35,7 @@ export default function Header() {
               Qo'llanma
             </Link>
             <Link
-              href="/contact"
+              href="#contact"
               className="text-gray-700 hover:text-blue-600 transition"
             >
               Bog'lanish
@@ -46,9 +46,10 @@ export default function Header() {
             href={telegramLink}
             target="_blank"
             rel="noopener noreferrer"
-            className="bg-linear-to-r from-blue-600 to-blue-700 text-white px-6 py-2.5 rounded-lg hover:shadow-lg transition-all duration-300 hover:scale-105"
+            className="bg-linear-to-r from-blue-600 to-blue-700 text-white px-3 md:px-6 py-2 md:py-2.5 rounded-lg hover:shadow-lg transition-all duration-300 hover:scale-105 text-sm md:text-base"
           >
-            Bepul konsultatsiya
+            <span className="hidden sm:inline">Bepul konsultatsiya</span>
+            <span className="sm:hidden">Konsultatsiya</span>
           </a>
         </nav>
       </header>
