7806770ed550e929a1d61e47cef8f17b4720e357
islome <islombekkamoliddinoff05@gmail.com>
2026-02-26 23:34:40 +0500

    minor changes with speacial blog section

---
+++ app/about/blog/warning/page.tsx
@@ -0,0 +1,10 @@
+import SpecialBlogSection from '@/components/ui/blog'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <SpecialBlogSection />
+    </div>
+  )
+}
