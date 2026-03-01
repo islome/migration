c15d640bf6e59462047923a973c7a49833bdaef2
islome <islombekkamoliddinoff05@gmail.com>
2026-03-01 12:28:09 +0500

    new secret api safed

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
-const client = new OpenAI({ apiKey: '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' });
+const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export async function POST(request) {
   try {
