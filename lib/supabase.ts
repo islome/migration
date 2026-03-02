b43e9f2266acfe3ef5c30799853a467abebef348
islome <islombekkamoliddinoff05@gmail.com>
2026-03-02 16:18:10 +0500

    minor changes

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://ojhqnvlgqlxyiipzzneq.supabase.co"
+const supabaseUrl = process.env.NEXT_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
