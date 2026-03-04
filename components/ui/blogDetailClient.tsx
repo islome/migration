54d5e02c01d21b5793a00223a7f05e99a636b583
islome <islombekkamoliddinoff05@gmail.com>
2026-03-04 17:44:56 +0500

    minor design changes

--- components/ui/blogDetailClient.tsx
+++ components/ui/blogDetailClient.tsx
@@ -26,9 +26,9 @@ export default function BlogDetailClient({ blog }: { blog: Blog }) {
 
   return (
     <div className="min-h-screen bg-white">
-      <div className="max-w-7xl mx-auto px-6 sm:px-10 lg:px-16 py-12 lg:py-20">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 xl:gap-20 items-start">
-          <div className="space-y-10 lg:sticky lg:top-12 lg:pr-8 xl:pr-12 pl-4 lg:pl-0 p-4">
+      <div className="max-w-7xl mx-auto px-6 sm:px-4 lg:px-8 py-12 lg:py-20">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 xl:gap-10 items-start">
+          <div className="space-y-10 lg:sticky lg:top-12 pl-4 lg:pl-0 p-4">
             <span
               className={`inline-block px-5 py-2.5 rounded-full text-base font-semibold shadow-sm ${cat?.color || "bg-blue-100 text-blue-800"}`}
             >
